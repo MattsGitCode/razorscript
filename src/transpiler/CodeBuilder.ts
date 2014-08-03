91f9efb45d50639f6bce68e403348a54ae1b712f
Matt Smith <m@ttsco.de>
2014-08-04 00:20:02 +0100

    escape carriage returns during transpilation

--- src/transpiler/CodeBuilder.ts
+++ src/transpiler/CodeBuilder.ts
@@ -4,7 +4,7 @@ enum CodeBuilderState {
 }
 
 function escape(val: string): string {
-  return val.replace('\"', '\\\"').replace('\n', '\\n');
+  return val.replace('\"', '\\\"').replace('\r', '\\r').replace('\n', '\\n');
 }
 
 class CodeBuilder {
